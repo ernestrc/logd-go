c666d3f13f1b57ed7f3f03a15ecda9e347d22d25
ernestrc <ernest@unstable.build>
2017-11-07 19:30:19 -0800

    remove ticker

--- cmd/smeagol/main.go
+++ cmd/smeagol/main.go
@@ -6,13 +6,11 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"time"
 
 	lua "github.com/Shopify/go-lua"
 	"github.com/opentok/blue/logging"
 )
 
-var tick = flag.Int("T", 1000, "duration of input buffering period in Microseconds. Default is 1000us")
 var script = flag.String("S", "", "Lua script that implements function log (time, level, flow, operation, step, properties). Defaults to printing a digest of the logs")
 var scriptMode = flag.String("M", "bt", "Lua script load mode which controls whether the chunk can be text or binary (that is, a precompiled chunk). It may be the string 'b' (only binary chunks), 't' (only text chunks), or 'bt' (both binary and text). The default is 'bt'.")
 
@@ -109,16 +107,12 @@ func loadLuaRuntime() *lua.State {
 func main() {
 	flag.Parse()
 
-	tick := time.Duration(*tick * 1000)
 	p := logging.NewParser()
 	reader := bufio.NewReader(os.Stdin)
-	ticker := time.NewTicker(tick)
 	logs := make([]logging.Log, 0)
 	l := loadLuaRuntime()
 	var buf [64 * 1000 * 1000]byte
 	for {
-		// time-based input buffering
-		<-ticker.C
 		n, err := reader.Read(buf[:])
 		if err != nil {
 			log.Fatal(err)
